97949348b80bc302cc42fbb581cc243b8d4d76b6
agustinizag <agustinizag1@gmail.com>
2025-12-12 22:53:59 -0300

    Implement simple expense_card

---
+++ lib/widgets/expense_card.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ExpenseCard extends StatelessWidget {
+  const ExpenseCard({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Text("Simple expense");
+  }
+}
