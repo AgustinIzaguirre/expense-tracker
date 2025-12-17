c8cd9ab8c9e204564a9d692a7e6b55eb355771a3
agustinizag <agustinizag1@gmail.com>
2025-12-17 20:53:58 -0300

    Add models for payment_method and cathegory

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,10 +1,13 @@
-class Expense {
-  String amount;
-  String description;
-  String paymentMethod;
-  String cathegory;
-  String date;
+import 'package:expense_tracker/models/cathegory.dart';
+import 'package:expense_tracker/models/payment_method.dart';
 
-  Expense(this.amount, this.description, this.paymentMethod, this.cathegory, this.date);
+class Expense {
+  final String amount;
+  final String description;
+  final PaymentMethod paymentMethod;
+  final Cathegory cathegory;
+  final DateTime date;
+
+  const Expense(this.amount, this.description, this.paymentMethod, this.cathegory, this.date);
 
 }
