cd8e05b60ed34f61e5de8d00513122bdea646445
agustinizag <agustinizag1@gmail.com>
2025-12-22 18:16:51 -0300

    Add seed for default configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 
+import 'package:expense_tracker/data/seeds/isar_categories_seed.dart';
+import 'package:expense_tracker/infraestructure/isar_instance.dart';
 import 'package:expense_tracker/pages/expenses/expenses_page.dart';
 import 'package:expense_tracker/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await IsarInstance.init();
+  await IsarCategorySeed.seedIfEmpty();
+
+  runApp(const ExpenseTrackerApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ExpenseTrackerApp extends StatelessWidget {
+  const ExpenseTrackerApp({super.key});
 
   // This widget is the root of your application.
   @override
